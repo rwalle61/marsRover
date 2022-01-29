0b5e914710b2dc4aa398dacac55b9efb839a033f
Richard Waller <richard.lh.waller@gmail.com>
2022-01-29 15:08:07 +0000

    refactor input parsing

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 const moveRovers = (input: string): string => {
-  if (input.split('\n').length === 1) {
+  const parsedInput = input.split('\n');
+
+  if (parsedInput.length === 1) {
     return '';
   }
-  if (input.split('\n')[2]) {
+
+  if (parsedInput[2]) {
     return '0 0 W';
   }
-  return input.split('\n')[1];
+
+  return parsedInput[1];
 };
 
 export default moveRovers;
