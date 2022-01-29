d663b8e6cca57ffdbe90744bdf355ced4d7d0ec1
Richard Waller <richard.lh.waller@gmail.com>
2022-01-29 14:44:20 +0000

    when input has one rover without instructions

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
-const moveRovers = (input: string) => '';
+const moveRovers = (input: string): string => {
+  if (input === '5 5') {
+    return '';
+  }
+  return '0 0 N';
+};
 
 export default moveRovers;
--- test/main.test.ts
+++ test/main.test.ts
@@ -11,6 +11,20 @@ describe('Acceptance', () => {
     });
   });
 
+  describe('when input has one rover without instructions', () => {
+    const input = `
+5 5
+0 0 N
+
+`;
+
+    it('should show the rover in the same position', () => {
+      const output = moveRovers(input);
+
+      expect(output).toBe('0 0 N');
+    });
+  });
+
   // eslint-disable-next-line jest/no-disabled-tests
   describe.skip('when input is the example test input', () => {
     const input = `
