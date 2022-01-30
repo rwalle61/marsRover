a8c6f102173ccee1bc57e6bb2094266c02a9eb3f
Richard Waller <richard.lh.waller@gmail.com>
2022-01-30 11:23:25 +0000

    add smaller acceptance tests

--- test/acceptance/main.test.ts
+++ test/acceptance/main.test.ts
@@ -1,7 +1,72 @@
 import moveRovers from '../../src/main';
 
-describe.skip('Acceptance', () => {
-  describe('when input is the example test input', () => {
+describe('Acceptance', () => {
+  describe('when input has a rover with one rotate instruction', () => {
+    const input = `
+5 5
+1 2 N
+L`.trim();
+
+    it('should move the rover to the expected position', () => {
+      const output = moveRovers(input);
+
+      expect(output).toBe('1 2 W');
+    });
+  });
+
+  describe('when input has a rover with one move instruction', () => {
+    const input = `
+5 5
+1 2 N
+M`.trim();
+
+    it('should move the rover to the expected position', () => {
+      const output = moveRovers(input);
+
+      expect(output).toBe('1 3 N');
+    });
+  });
+
+  describe('when input has a rover with a move and rotate instruction', () => {
+    const input = `
+5 5
+1 2 N
+LM`.trim();
+
+    it('should move the rover to the expected position', () => {
+      const output = moveRovers(input);
+
+      expect(output).toBe('0 2 W');
+    });
+  });
+
+  describe('when input has the first example rover with instructions', () => {
+    const input = `
+5 5
+1 2 N
+LMLMLMLMM`.trim();
+
+    it('should move the rover to the expected position', () => {
+      const output = moveRovers(input);
+
+      expect(output).toBe('1 3 N');
+    });
+  });
+
+  describe('when input has the second example rover with instructions', () => {
+    const input = `
+5 5
+3 3 E
+MMRMMRMRRM`.trim();
+
+    it('should move the rover to the expected position', () => {
+      const output = moveRovers(input);
+
+      expect(output).toBe('5 1 E');
+    });
+  });
+
+  describe.skip('when input is the example test input', () => {
     const input = `
 5 5
 1 2 N
